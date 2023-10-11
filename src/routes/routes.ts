c72e2ea323b04a43561b765fe9c0b48bd0c2e611
Alejandro Silva <alejoyodax@gmail.com>
2023-10-10 21:27:05 -0500

    rutas corregidas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -16,15 +16,15 @@ export const routes: Route[] = [
 		name: 'Lazy-1'
 	},
 	{
-		to: '/lazy1',
-		path: 'lazy1',
-		Component: LazyPage1,
-		name: 'Lazy-1'
+		to: '/lazy2',
+		path: 'lazy2',
+		Component: LazyPage2,
+		name: 'Lazy-2'
 	},
 	{
-		to: '/lazy1',
-		path: 'lazy1',
-		Component: LazyPage1,
-		name: 'Lazy-1'
+		to: '/lazy3',
+		path: 'lazy3',
+		Component: LazyPage3,
+		name: 'Lazy-3'
 	}
 ];
